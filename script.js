2830388032fcae4a2144bf94e2459256bbf5b5b1
The-bAk-Dev <hamadi.the.music.therapist@gmail.com>
2024-10-14 15:19:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// START HERO SLIDER the JS PART  **************************
+
+// ici j'utiliserai var car j'utiliserai encore une fois la même variable avec le même nom swiper
+var swiper = new Swiper('.heroSwiper', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: true,
+  effect: 'fade',
+});
+
+// END HERO SLIDER the JS PART  **************************
+
+// START Categories SLIDER the JS PART  **************************
+
+// ici j'utiliserai var car j'utiliserai encore une fois la même variable avec le même nom swiper
+var swiper = new Swiper('.categorySwiper', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: true,
+});
+
+// END Categories SLIDER the JS PART  **************************
